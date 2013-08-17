c5a91631b493a061d161310f7a080d151f03cf88
okayama <kenmin.okayama@gmail.com>
2013-08-17 16:03:53 +0900

    Minor update, .pl to config.yaml, and others.

---
+++ plugins/RebuildParentIndex/lib/RebuildParentIndex/Callbacks.pm
@@ -0,0 +1,22 @@
+package RebuildParentIndex::Callbacks;
+use strict;
+
+use MT::Util qw( start_background_task );
+
+sub _cb_cms_post_save_entry {
+	my ( $eh, $app, $obj, $original ) = @_;
+	my $plugin = MT->component( 'RebuildParentIndex' );
+	if ( $plugin->get_config_value( 'only_pulish_status', 'blog:' . $obj->blog_id ) ) {
+MT->log( 'test' );
+	    return 1 unless $obj->status eq MT::Entry::RELEASE();
+	}
+	if ( my $blog = $obj->blog ) {
+        if ( my $website = $blog->website ) {
+            my $pub = MT::WeblogPublisher->new;
+            start_background_task( sub { $pub->rebuild_indexes( Blog => $website ); } );
+        }
+	}
+    return 1;
+}
+
+1;
--- plugins/RebuildParentIndex/lib/RebuildParentIndex/L10N/ja.pm
+++ plugins/RebuildParentIndex/lib/RebuildParentIndex/L10N/ja.pm
@@ -5,11 +5,12 @@ use vars qw( %Lexicon );
 
 our %Lexicon = (
     '_PLUGIN_DESCRIPTION' => &_plugin_description,
+    'only publish status' => '公開状態の場合のみ親ウェブサイトの再構築を行う',
 );
 
 sub _plugin_description {
     return<<'TEXT';
-ブログ記事またはウェブページが保存された際に、親ウェブサイト(保存が行われたのがウェブサイトの場合は自分自身)のインデックステンプレートを再構築します。mt-config.cgi に環境変数 <a href="http://www.movabletype.jp/documentation/appendices/config-directives/launchbackgroundtasks.html">LaunchBackGroundTasks</a> を設定しておけば再構築をバックグラウンドで行わせることができるので、見た目上の負荷は変わりません。<br />
+ブログ記事またはウェブページが保存された際に、親ウェブサイトのインデックステンプレートを再構築します。mt-config.cgi に環境変数 <a href="http://www.movabletype.jp/documentation/appendices/config-directives/launchbackgroundtasks.html">LaunchBackGroundTasks</a> を設定しておけば再構築をバックグラウンドで行わせることができます。
 TEXT
 }
 
